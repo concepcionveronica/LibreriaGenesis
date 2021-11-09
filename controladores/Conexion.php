9c966e843bd0d8624b44b271e7bf26152d21bf53
Reese Lovato <reesemlovato@gmail.com>
2021-11-09 09:16:46 -0600

    Agrega archivos del proyecto orignal
    Falta adaptarlos a la nueva estructura del proyecto
    
     On branch main
     Changes to be committed:
    	new file:   controladores/Conexion.php
    	new file:   controladores/Controladorcliente.php
    	new file:   vistas/insertarCliente.php

---
+++ controladores/Conexion.php
@@ -0,0 +1,44 @@
+<?php 
+define('DATABASE','dblibreria');
+define('HOST','localhost');
+define('USERNAME','root');
+define('PASSWORD','');
+class Conexion
+{
+    function obtene_conexion(){
+    try {
+    $pdo = new PDO(
+    'mysql:dbname='.DATABASE.
+    ';host='.HOST.
+    ';port:3306',
+    USERNAME,
+    PASSWORD,
+    array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
+    );
+    return $pdo;
+    } catch (Exception $e) {
+    return $e->getMessage().$e->getLine();
+
+
+    }
+    }
+
+
+    function cerrar_sesion($pdo){
+    $pdo=null;
+    }
+}
+
+/*
+$instancia = new Conexion();
+$conectar = $instancia->obtene_conexion();
+
+
+$sql = "SELECT *FROM usuario";
+$statement = $conectar->prepare($sql);
+$statement->execute();
+$datos = $statement->fetchAll();
+
+
+print_r($datos);*/
+?>
